5ec36ea98638e1da9a1fd049e2047e2c2a3d688f
Anthony Chin <wei-wing.chin@mail.mcgill.ca>
2015-11-21 13:20:05 -0500

    bmp to 2d and vice versa + rotate 180

--- MobileImageProcessing/app/src/main/java/mobileimageprocessing/com/mobileimageprocessing/MainActivity.java
+++ MobileImageProcessing/app/src/main/java/mobileimageprocessing/com/mobileimageprocessing/MainActivity.java
@@ -95,14 +95,25 @@ public class MainActivity extends AppCompatActivity {
 //                    String path = "content://"+selectedImageUri.getAuthority()+""+selectedImageUri.getPath();
 //                    System.out.println(path);
                     Bitmap btemp = BitmapFactory.decodeStream(iStream);
-                    imageDisplay.setImageBitmap(btemp);
+
+                    int[][] seqRes = rotateImageOneEighty(arrayFromBitmap(btemp), btemp.getHeight(), btemp.getWidth());
+                    Bitmap bitmapOutput = bitmapFromArray(seqRes);
+
+                    imageDisplay.setImageBitmap(bitmapOutput);
                     System.out.println("works");
                     /// use btemp Image file
                     RadioGroup rGroup = (RadioGroup)findViewById(R.id.radioGroup);
-                    Intent processIntent = new Intent(this.getApplicationContext(), BaseImageProcessingActivity.class);
-                    //processIntent.putExtra(this.EXTRA_MESSAGE, btemp);
-                    bitmap = btemp;
-                    startActivityForResult(processIntent,2);
+//                    Intent processIntent = new Intent(this.getApplicationContext(), BaseImageProcessingActivity.class);
+//                    //processIntent.putExtra(this.EXTRA_MESSAGE, btemp);
+//                    bitmap = bitmapOutput;
+//                    startActivityForResult(processIntent,2);
+
+//                    int[][] seqRes = rotateImageClockwise(arrayFromBitmap(btemp), btemp.getHeight(), btemp.getWidth());
+//                    Bitmap bitmapOutput = bitmapFromArray(seqRes);
+//
+//                    imageDisplay.setImageBitmap(bitmapOutput);
+                    /// use btemp Image file
+
 // This will get the radiobutton in the radiogroup that is checked
 //                    RadioButton checkedRadioButton = (RadioButton)rGroup.findViewById(rGroup.getCheckedRadioButtonId());
 //                    switch(checkedRadioButton.getId()){
@@ -117,15 +128,112 @@ public class MainActivity extends AppCompatActivity {
 //                    }
                 }
                 break;
-            case 2:
-                if(data!=null){
-//                    Bitmap btemp = data.getParcelableExtra(this.EXTRA_MESSAGE);
-//                    Bitmap btemp = BaseImageProcessingActivity.bOutput;
-                    imageDisplay.setImageBitmap(input);
-
-
-                }
+//            case 2:
+//                if(data!=null){
+////                    Bitmap btemp = data.getParcelableExtra(this.EXTRA_MESSAGE);
+////                    Bitmap btemp = BaseImageProcessingActivity.bOutput;
+//                    imageDisplay.setImageBitmap(input);
+//
+//
+//                }
         }
 
     }
+//
+//    public int[][] rotateImageClockwise(int[][] image, int height, int width){
+//
+//        int[][] newImage = new int[width][height];
+//
+//        for (int i = 0; i < width; i++) {
+//            for (int j = 0; j < height; j++) {
+//                newImage[i][j] = image[i][j];
+//                System.out.println(newImage[i][j]);
+//            }
+//            System.out.println("\n");
+//
+//        }
+//
+//        return newImage;
+//    }
+
+    public int[][] rotateImageOneEighty(int[][] image, int height, int width){
+        int newHeight = width;
+        int newWidth = height;
+        System.out.println(width); //3264
+        System.out.println(height); //2448
+
+        int[][] newImage = new int[newWidth][newHeight];
+
+        for (int i = 0; i < newWidth; i++) {
+            for (int j = 0; j < newHeight; j++) {
+//                System.out.print("(" + i + "," + j + ")");
+//                newImage[i][j] = image[width-1-j][i];
+                newImage[i][j] = image[j][i];
+            }
+//            System.out.println("");
+        }
+
+        return newImage;
+    }
+
+    public static Bitmap bitmapFromArray(int[][] array) {
+
+        Bitmap.Config conf = Bitmap.Config.ARGB_8888; // see other conf types
+        Bitmap bmp = Bitmap.createBitmap(array.length, array[0].length, conf);
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                bmp.setPixel(i, j, array[i][j]);
+            }
+        }
+        return bmp;
+    }
+
+    public static int[][] arrayFromBitmap(Bitmap map) {
+        int width = map.getWidth();
+        int height = map.getHeight();
+        int[][] result = new int[width][height];
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                result[i][j] = map.getPixel(i, j);
+            }
+        }
+        return result;
+
+    }
+
+//    public static Bitmap bitmapFromArray(int[][] pixels2d){
+//        int width = pixels2d.length;
+//        int height = pixels2d[0].length;
+//        int[] pixels = new int[width * height];
+//        int pixelsIndex = 0;
+//        for (int i = 0; i < width; i++)
+//        {
+//            for (int j = 0; j < height; j++)
+//            {
+//                pixels[pixelsIndex] = pixels2d[i][j];
+//                pixelsIndex ++;
+//            }
+//        }
+//        return Bitmap.createBitmap(pixels, width, height, Bitmap.Config.ARGB_8888);
+//    }
+//
+//    public static int[][] arrayFromBitmap(Bitmap source){
+//        int width = source.getWidth();
+//        int height = source.getHeight();
+//        int[][] result = new int[width][height];
+//        int[] pixels = new int[width*height];
+//        source.getPixels(pixels, 0, width, 0, 0, width, height);
+//        int pixelsIndex = 0;
+//        for (int i = 0; i < width; i++)
+//        {
+//            for (int j = 0; j < height; j++)
+//            {
+//                result[i][j] =  pixels[pixelsIndex];
+//                pixelsIndex++;
+//            }
+//        }
+//        return result;
+//    }
 }
