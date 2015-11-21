9175fa7dbf05685bb95a8d2a1c5f77b2f60ec37e
arthurkam <arthur.kam@hotmail.com>
2015-11-20 21:41:25 -0500

    Add base code to get image from IO

---
+++ MobileImageProcessing/app/src/main/java/mobileimageprocessing/com/mobileimageprocessing/BaseImageProcessingActivity.java
@@ -0,0 +1,67 @@
+package mobileimageprocessing.com.mobileimageprocessing;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class BaseImageProcessingActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "mobileimageprocessing.com.mobileimageprocessing.imageResult";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//            setContentView(R.layout.activity_base_image_processing);
+        Intent intent = getIntent();
+        Bitmap bitmap = intent.getParcelableExtra(EXTRA_MESSAGE);
+        int[][] seqRes = processImageSequential(arrayFromBitmap(bitmap));
+        Bitmap bitmapOutput = bitmapFromArray(seqRes);
+
+        Intent output = new Intent();
+        output.putExtra(this.EXTRA_MESSAGE, bitmapOutput);
+        setResult(RESULT_OK, output);
+        finish();
+
+    }
+    private int[][] processImageSequential(int[][] image){
+        return image;
+    }
+    public static Bitmap bitmapFromArray(int[][] pixels2d){
+        int width = pixels2d.length;
+        int height = pixels2d[0].length;
+        int[] pixels = new int[width * height];
+        int pixelsIndex = 0;
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                pixels[pixelsIndex] = pixels2d[i][j];
+                pixelsIndex ++;
+            }
+        }
+        return Bitmap.createBitmap(pixels, width, height, Bitmap.Config.ARGB_8888);
+    }
+
+    public static int[][] arrayFromBitmap(Bitmap source){
+        int width = source.getWidth();
+        int height = source.getHeight();
+        int[][] result = new int[width][height];
+        int[] pixels = new int[width*height];
+        source.getPixels(pixels, 0, width, 0, 0, width, height);
+        int pixelsIndex = 0;
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                result[i][j] =  pixels[pixelsIndex];
+                pixelsIndex++;
+            }
+        }
+        return result;
+    }
+
+}
--- MobileImageProcessing/app/src/main/java/mobileimageprocessing/com/mobileimageprocessing/MainActivity.java
+++ MobileImageProcessing/app/src/main/java/mobileimageprocessing/com/mobileimageprocessing/MainActivity.java
@@ -1,31 +1,52 @@
 package mobileimageprocessing.com.mobileimageprocessing;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button imageGallery;
+    ImageView imageDisplay;
+    public static final String EXTRA_MESSAGE = "mobileimageprocessing.com.mobileimageprocessing.imageResult";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = ((Toolbar) findViewById(R.id.toolbar));
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        imageGallery = ((Button) findViewById(R.id.btnGallery));
+        imageGallery.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+
+                Intent intent = new Intent();
+//                Util.DogDye = false;
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(
+                        Intent.createChooser(intent, "Complete action using"), 1);
+
             }
         });
+        imageDisplay = ((ImageView) findViewById(R.id.imageView));
     }
 
     @Override
@@ -48,5 +69,54 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+         if (resultCode != RESULT_OK) return;
+
+        switch (requestCode) {
+
+            case 1:
+                if (data != null) {
+                    Uri selectedImageUri = data.getData();
+                    //String selectedImagePath = getPath(selectedImageUri);
+                    InputStream iStream = null;
+                    try {
+                        iStream = getContentResolver().openInputStream(selectedImageUri);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    BitmapFactory.Options options = new BitmapFactory.Options();
+                    options.inJustDecodeBounds = true;
+//                    System.out.println(selectedImagePath);
+//                    String path = "content://"+selectedImageUri.getAuthority()+""+selectedImageUri.getPath();
+//                    System.out.println(path);
+                    Bitmap btemp = BitmapFactory.decodeStream(iStream);
+                    imageDisplay.setImageBitmap(btemp);
+                    /// use btemp Image file
+                    RadioGroup rGroup = (RadioGroup)findViewById(R.id.radioGroup);
+// This will get the radiobutton in the radiogroup that is checked
+//                    RadioButton checkedRadioButton = (RadioButton)rGroup.findViewById(rGroup.getCheckedRadioButtonId());
+//                    switch(checkedRadioButton.getId()){
+//                        case R.id.radioButton:
+//                            break;
+//                        case R.id.radioButton2:
+//                            break;
+//                        case R.id.radioButton3:
+//                            break;
+//                        default:
+//                            return;
+//                    }
+                }
+                break;
+            case 2:
+                if(data!=null){
+                    Bitmap btemp = data.getParcelableExtra(this.EXTRA_MESSAGE);
+                    imageDisplay.setImageBitmap(btemp);
+
+                }
+        }
+
     }
 }
