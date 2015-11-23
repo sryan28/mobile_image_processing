dbce237fb2f67ea82b92cfff953847599d66b3f6
sryan28 <sean.ryan3@mail.mcgill.ca>
2015-11-23 15:43:02 -0500

    seq and currently doing threading

--- MobileImageProcessing/app/src/main/java/mobileimageprocessing/com/mobileimageprocessing/ScaleImageProcessingActivity.java
+++ MobileImageProcessing/app/src/main/java/mobileimageprocessing/com/mobileimageprocessing/ScaleImageProcessingActivity.java
@@ -9,89 +9,7 @@ public class ScaleImageProcessingActivity extends BaseImageProcessingActivity{
 
     @Override
     public int[][] processImageSequential(int[][] image ) {
-        return scaleImagePixelate(image, (int) (image.length), (int) (image[0].length));
-    }
-
-    public int[][] scaleImagePixelate(int [][] image, int width, int height){
-        System.out.println("pixeal scale called");
-        double scale = 0.5;
-        int X_BOX = (int)(1/scale);
-        int Y_BOX = (int)(1/scale);
-        System.out.println(X_BOX);
-        System.out.println(Y_BOX);
-
-        int[][] image2 = new int[image.length/2][image[0].length/2];
-        int xCounter=0;
-        int yCounter=0;
-        for (int i = 0; i < image.length; i += X_BOX) {
-            int xBound = i + X_BOX > image.length ? image.length : i + X_BOX;
-            yCounter=0;
-            for (int j = 0; j < image[i].length; j += Y_BOX) {
-                int redSum = 0;
-                int greenSum = 0;
-                int blueSum = 0;
-                int yBound = j + Y_BOX > image[0].length ? image[0].length : j + Y_BOX;
-                int counter = 0;
-                for (int x = i; x < xBound; x++) {
-                    for (int y = j; y < yBound; y++) {
-                        redSum += Color.red(image[x][y]);
-                        greenSum += Color.green(image[x][y]);
-                        blueSum += Color.blue(image[x][y]);
-                        counter++;
-                    }
-                }
-                redSum/=counter;
-                greenSum/=counter;
-                blueSum/=counter;
-
-                image2[xCounter][yCounter] = Color.argb(255,redSum,greenSum,blueSum);
-                yCounter++;
-            }
-            xCounter++;
-        }
-
-        return image2;
-    }
-    public int[][] scaleImage(int[][] image, int width, int height) {
-        int newWidth = width;
-        int newHeight = height;
-
-        int [][] result = new int[newWidth][newHeight];
-
-        int origHeight = image[0].length;
-        int origWidth = image.length;
-
-        int [] temp = new int[ origWidth *  origHeight];
-        int [] newtmp = new int[(newWidth * newHeight)];
-
-        int k = 0;
-        for(int i = 0; i < origWidth; i++) {
-            for(int j = 0; j < origHeight; j++) {
-                temp[k] = image[i][j];
-                k++;
-            }
-        }
-
-        double xratio = (double)origWidth / (double)newWidth;
-        double yratio = (double)origHeight/ (double)newHeight;
-
-        //SCALING DONE HERE
-        double px, py;
-        for(int i = 0; i < newHeight; i++) {
-            for(int j = 0; j < newWidth; j++) {
-                px = Math.floor(j * xratio);
-                py = Math.floor(i * yratio);
-                newtmp[( i * newWidth) + j] = temp[(int) ((py * origWidth) + px)];
-            }
-        }
-        //CONVERT BACK TO 2D
-        //int x = 0;
-        for(int i = 0, x = 0; i < newWidth; i++) {
-            for (int j = 0; j < newHeight; j++) {
-                result[i][j] = newtmp[x++];
-            }
-        }
-        return result;
+        return resize(image, (int) Math.floor(image.length * 0.5), (int) Math.floor(image[0].length * 0.5));
     }
 
     public int [][] resize(int [][] image, int w, int h) {
@@ -137,7 +55,88 @@ public class ScaleImageProcessingActivity extends BaseImageProcessingActivity{
                 result[i][j] = Color.rgb(red, green, blue);
             }
         }
-        System.out.printf("now size %d, %d", result.length, result[0].length);
+//        System.out.printf("now size %d, %d", result.length, result[0].length);
         return result;
     }
+
+    public int [][] resizeThreads(int [][] image, int w, int h, int threadCount) {
+        Thread[] threads = new Thread[threadCount];
+        int sr = image.length / w;
+        int sc = image[0].length / h;
+
+        int fragsPerThread = image.length / threadCount;
+
+        for(int i = 0; i < threadCount; i++) {
+            threads[i] = new Thread(new ScalingRunnable(image, start, end, 0, image[0].length, sr, sc));
+            threads[i].start();
+        }
+
+        for(int i = 0; i < threadCount; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return image;
+    }
+
+    private class ScalingRunnable implements Runnable {
+        int [][] image;
+        int xStart;
+        int xEnd;
+        int yStart;
+        int yEnd;
+        int sr;
+        int sc;
+
+        ScalingRunnable(int [][] image, int xStart, int xEnd, int yStart, int yEnd, int sr, int sc) {
+            this.image = image;
+            this.xStart = xStart;
+            this.xEnd = xEnd;
+            this.yStart = yStart;
+            this.yEnd = yEnd;
+            this.sr = sr;
+            this.sc = sc;
+        }
+
+        @Override
+        public void run() {
+            double rf = 0, cf = 0;
+            for(int i = xStart; i < xEnd; i ++) {
+                for (int j = yStart; j < yEnd; j++) {
+                    rf = i * sr;
+                    cf = j * sc;
+
+                    int r = (int) Math.floor(rf);
+                    int c = (int) Math.floor(cf);
+
+                    int deltar = (int) Math.floor(rf - r);
+                    int deltac = (int) Math.floor(cf - c);
+
+                    int red = 0;
+                    int green = 0;
+                    int blue = 0;
+
+                    red = Color.red(image[r][c]) * (1 - deltar) * (1 - deltac)
+                            + Color.red(image[r + 1][c] * deltar * (1 - deltac))
+                            + Color.red(image[r][c + 1]) * (1 - deltar) * deltac
+                            + Color.red(image[r + 1][c + 1]) * deltac * deltar;
+
+                    green = Color.green(image[r][c]) * (1 - deltar) * (1 - deltac)
+                            + Color.green(image[r + 1][c] * deltar * (1 - deltac))
+                            + Color.green(image[r][c + 1]) * (1 - deltar) * deltac
+                            + Color.green(image[r + 1][c + 1]) * deltac * deltar;
+
+                    blue = Color.blue(image[r][c]) * (1 - deltar) * (1 - deltac)
+                            + Color.blue(image[r + 1][c] * deltar * (1 - deltac))
+                            + Color.blue(image[r][c + 1]) * (1 - deltar) * deltac
+                            + Color.blue(image[r + 1][c + 1]) * deltac * deltar;
+
+                    image[i][j] = Color.rgb(red, green, blue);
+                }
+            }
+        }
+    }
 }
+
