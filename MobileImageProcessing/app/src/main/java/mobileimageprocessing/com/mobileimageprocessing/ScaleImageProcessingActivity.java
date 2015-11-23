4fa49edced9586dcc65935b35f7bf7504d4b8be6
sryan28 <sean.ryan3@mail.mcgill.ca>
2015-11-23 00:55:01 -0500

    new resize method

--- MobileImageProcessing/app/src/main/java/mobileimageprocessing/com/mobileimageprocessing/ScaleImageProcessingActivity.java
+++ MobileImageProcessing/app/src/main/java/mobileimageprocessing/com/mobileimageprocessing/ScaleImageProcessingActivity.java
@@ -93,4 +93,51 @@ public class ScaleImageProcessingActivity extends BaseImageProcessingActivity{
         }
         return result;
     }
+
+    public int [][] resize(int [][] image, int w, int h) {
+        int origHeight = image[0].length;
+        int origWidth = image.length;
+
+        double sr = origWidth / w;
+        double sc = origHeight / h;
+
+        int [][] result = new int[w][h];
+
+        double rf = 0, cf = 0;
+        for(int i = 0; i < w; i ++) {
+            for(int j = 0; j < h; j++) {
+                rf = i * sr;
+                cf = j * sc;
+
+                int r = (int) Math.floor(rf);
+                int c = (int) Math.floor(cf);
+
+                int deltar = (int) Math.floor(rf - r);
+                int deltac = (int) Math.floor(cf - c);
+
+                int red = 0;
+                int green = 0;
+                int blue = 0;
+
+                red = Color.red(image[r][c]) * (1 - deltar) * (1 - deltac)
+                        + Color.red(image[r + 1][c] * deltar * (1 - deltac))
+                        + Color.red(image[r][c + 1]) * (1 - deltar) * deltac
+                        + Color.red(image[r + 1][c + 1]) * deltac * deltar;
+
+                green = Color.green(image[r][c]) * (1 - deltar) * (1 - deltac)
+                        + Color.green(image[r + 1][c] * deltar * (1 - deltac))
+                        + Color.green(image[r][c + 1]) * (1 - deltar) * deltac
+                        + Color.green(image[r + 1][c + 1]) * deltac * deltar;
+
+                blue = Color.blue(image[r][c]) * (1 - deltar) * (1 - deltac)
+                        + Color.blue(image[r + 1][c] * deltar * (1 - deltac))
+                        + Color.blue(image[r][c + 1]) * (1 - deltar) * deltac
+                        + Color.blue(image[r + 1][c + 1]) * deltac * deltar;
+
+                result[i][j] = Color.rgb(red, green, blue);
+            }
+        }
+        System.out.printf("now size %d, %d", result.length, result[0].length);
+        return result;
+    }
 }
